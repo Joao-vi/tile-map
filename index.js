628c2cc6d8724a617f2a460440427b53e801a8e0
joao-vi <joaovictor.go.ro@gmail.com>
2022-05-28 11:59:41 -0400

    feat: v4

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+const pan = window.Panzoom;
+
+console.log(pan);
 const canvas = document.querySelector("canvas");
 const tileSetContainer = document.querySelector(".tileset-container");
 const tileSetImg = document.querySelector("#tileset-img");
 const tileSetSelection = document.querySelector(".tile-selecting");
-
-let selection = [0, 0];
 const ctx = canvas.getContext("2d");
 
-ctx.width = window.innerWidth;
+let selection = [0, 0];
+
+canvas.width = window.innerWidth - 40;
 ctx.height = 400;
 ctx.lineWidth = 2;
 ctx.lineJoin = "round";
 let currentLayer = 0;
 
-// let layers = [{ "0-0": [0, 0] }, {}];
+const padding = 2;
+const tileSize = 32;
 
-let layers = [[Math.floor(ctx.width / 32), Math.floor(ctx.height / 32)], []];
+let layers = [
+  [Math.floor(canvas.width / 32), Math.floor(canvas.height / 32)],
+  [{}],
+];
+
 tileSetImg.src =
   "https://assets.codepen.io/21542/TileEditorSpritesheet.2x_2.png";
 
@@ -22,52 +30,67 @@ tileSetImg.onload = () => {
   draw();
 };
 
+// const panzoom = Panzoom(canvas, {
+//   maxScale: 1,
+// });
+// panzoom.pan(10, 10);
+// panzoom.zoom(2, { animate: true });
+
+function generateBackground() {
+  for (let i = 0; i < canvas.width / tileSize; i++) {
+    for (let j = 0; j < canvas.width / tileSize; j++) {
+      ctx.fillStyle = "#cccccc";
+      ctx.fillRect(
+        i * tileSize,
+        j * tileSize,
+        tileSize - padding,
+        tileSize - padding
+      );
+    }
+  }
+}
+
+function paintTile(props) {
+  const { x = 0, px = 0, y = 0, py = 0, wx = padding, hy = padding } = props;
+  return ctx.fillRect(
+    x * tileSize - px,
+    y * tileSize - py,
+    tileSize - wx,
+    tileSize - hy
+  );
+}
+
+function generateTiles(layer) {
+  layer.forEach(({ x, y, color }) => {
+    const hasSibilingX = layer?.some(
+      (tile) => tile.x === x - 1 && tile.y === y
+    );
+    const hasSibilingY = layer?.some(
+      (tile) => tile.y === y - 1 && tile.x === x
+    );
+    ctx.fillStyle = color;
+    if (hasSibilingX && hasSibilingY) {
+      paintTile({ x, px: 2, y, py: 2, wx: 0, hy: 0 });
+    } else if (hasSibilingX) {
+      paintTile({ x, px: 2, y, wx: 0 });
+    } else if (hasSibilingY) {
+      paintTile({ x, y, py: 2, hy: 0 });
+    } else {
+      paintTile({ x, y });
+    }
+  });
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  var sizeOfCrop = 32;
-
   layers.forEach((layer, index) => {
     if (index === 0) {
-      for (let i = 0; i < layer[0]; i++) {
-        for (let j = 0; j < layer[0]; j++) {
-          ctx.fillStyle = "#cccccc";
-          ctx.fillRect(i * 32, j * 32, 30, 30);
-        }
-      }
+      generateBackground();
     } else {
-      layer.forEach(({ x, y, color }) => {
-        // const hasLeft = layer?.some((tile) => tile.x === x - 1);
-        // const hasRight = layer?.some((tile) => tile.x === x + 1);
-        // if (hasLeft) {
-        //   ctx.fillRect(x * 32 - 2, y * 32, 32, 30);
-        // } else {
-        // }
-        ctx.fillStyle = color;
-        ctx.fillRect(x * 32, y * 32, 30, 30);
-      });
+      generateTiles(layer);
     }
   });
-
-  // layers.forEach((layer) => {
-  //   Object.keys(layer).forEach((key) => {
-  //     const positionX = Number(key.split("-")[0]);
-  //     const positionY = Number(key.split("-")[1]);
-
-  //     const [tilesheetX, tilesheetY] = layer[key];
-  //     ctx.drawImage(
-  //       tileSetImg,
-  //       tilesheetX * 32, // tilesheet x,y (top left corner of the grab)
-  //       tilesheetY * 32, // tilesheet x,y (top left corner of the grab)
-  //       sizeOfCrop, // how big of a grab
-  //       sizeOfCrop, // how big of a grab
-  //       positionX * 32, // where you want the crop to be placed
-  //       positionY * 32, // where you want the crop to be placed
-  //       sizeOfCrop, // size of placement of what was grabbed
-  //       sizeOfCrop // size of placement of what was grabbed
-  //     );
-  //   });
-  // });
 }
 
 function getCoords(e) {
@@ -75,13 +98,13 @@ function getCoords(e) {
   const mouseX = e.clientX - x;
   const mouseY = e.clientY - y;
 
-  return [Math.floor(mouseX / 32), Math.floor(mouseY / 32)];
+  return [Math.floor(mouseX / tileSize), Math.floor(mouseY / tileSize)];
 }
 
 tileSetContainer.addEventListener("mousedown", (e) => {
   selection = getCoords(e);
-  tileSetSelection.style.left = selection[0] * 32 + "px";
-  tileSetSelection.style.top = selection[1] * 32 + "px";
+  tileSetSelection.style.left = selection[0] * tileSize + "px";
+  tileSetSelection.style.top = selection[1] * tileSize + "px";
 });
 
 let isMouseDown = false;
@@ -90,22 +113,18 @@ canvas.addEventListener("mousedown", () => (isMouseDown = true));
 canvas.addEventListener("mouseup", () => (isMouseDown = false));
 canvas.addEventListener("mouseleave", () => (isMouseDown = false));
 
-// window.addEventListener("resize", () => {
-//   canvas.width = window.innerWidth;
-//   draw();
-// });
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth - 40;
+  draw();
+});
 
 canvas.addEventListener("mousedown", addTile);
 canvas.addEventListener("mousemove", (e) => {
-  // layers[1] = {};
-
   if (isMouseDown) {
     currentLayer = 0;
 
     addTile(e);
   }
-  // currentLayer = 1;
-  // addTile(e);
 
   hover(e);
 });
@@ -117,8 +136,18 @@ function hover(e) {
   draw();
   ctx.strokeStyle = "#414040";
   ctx.fillStyle = "rgba(226, 224, 224, 0.2)";
-  ctx.strokeRect(x * 32, y * 32, 30, 30);
-  ctx.fillRect(x * 32 + 2, y * 32 + 2, 26, 26);
+  ctx.strokeRect(
+    x * tileSize,
+    y * tileSize,
+    tileSize - padding,
+    tileSize - padding
+  );
+  ctx.fillRect(
+    x * tileSize + padding,
+    y * tileSize + padding,
+    tileSize - 2 * padding,
+    tileSize - 2 * padding
+  );
 }
 
 function addTile(e) {
